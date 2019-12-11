afdc39e4146928e01d7c56a26677b741626f4c5f
mnzolo <mnzolo@student.wethinkcode.co.za>
2019-12-11 18:04:22 +0200

    README.md

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const bodyparser = require('body-parser');
+const formidable = require('formidable');
+const fs = require('fs');
+
 
 app.use(express.static("profimages"));
 
@@ -11,21 +14,53 @@ app.post("/profile.html", function (req, res) {
         res.send("Profile is Being Changed")
     }
     else if (req.body.About == "About") {
-        res.send("About is Being Changed")
+        console.log("About is Being Changed")
+        res.sendFile(__dirname + "/edit_profile.html");
     }
-     else if (req.body.Details == "Details") {
-        res.send("Personal Details Are Being Changed")
+    else if (req.body.Details == "Details") {
+        console.log("Personal Details Are Being Changed")
+        res.sendFile(__dirname + "/edit_profile.html");
     }
-    else if (req.body.Take == "Take")
-    {
-        res.send("Taking image");
+    else if (req.body.Take == "Take") {
+        console.log("Taking image");
+        res.sendFile(__dirname + "/profile.html");
     }
-    else if (req.body.upload == undefined)
-    {
-        res.send("found image");
+    else if (req.body.upload == undefined) {
+        var form = new formidable.IncomingForm();
+
+        form.parse(req, function (err, fields, files) {
+            var oldpath = files.fileuploaded.path;
+            var newpath = __dirname + "/profimages/" + files.fileuploaded.name;
+            fs.rename(oldpath, newpath, function(err)
+            {
+                if(err) throw err;
+                console.log(newpath);
+                console.log("file has been moved");
+            }
+            );
+            res.end();
+        });
     }
 }
 );
+
+/*  app.post("/uploadprofile.html", function(req, res)
+{
+    var form = new formidable.IncomingForm();
+
+        form.parse(req, function (err, fields, files) {
+            var oldpath = files.fileuploaded.path;
+            var newpath = __dirname + "/profimages/" + files.fileuploaded.name;
+            fs.rename(oldpath, newpath, function(err)
+            {
+                if(err) throw err;
+                console.log(newpath);
+                console.log("file has been moved");
+            }
+            );
+            res.end();
+        });
+}); */
 
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/profile.html");
